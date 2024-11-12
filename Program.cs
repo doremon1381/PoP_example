1a844088c8c937e20c695479e58d919a56c10ea3
doremon1381 <doremon1381@gmail.com>
2024-11-12 16:55:05 +0700

    - for test PoP authentication

---
+++ Program.cs
@@ -0,0 +1,149 @@
+﻿using System.Net.Security;
+using System.Net.Sockets;
+using System.Net;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace PoP_example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                //Console.WriteLine("Hello, World!");
+                var server = Task.Factory.StartNew(() => new Server());
+                var client = Task.Factory.StartNew(() => new Client()).GetAwaiter().GetResult();
+
+                for (int i = 0; i < 1; i++)
+                {
+                    Task.Factory.StartNew(() =>
+                    {
+                        client.SendMessage();
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Client
+    {
+        // TODO: 1. create RSA private key 
+        //     : 2. call to server to get handshake and public key
+        //     : 3. random generate a pre-master secret
+        //     :    encrypt the secret with private key
+        //     :    send to server.
+        //     : 4. 
+
+        private RSAParameters _privateKey;
+
+        public Client()
+        {
+            _privateKey = RSAEncryptUtilities.GeneratePrivateAndPublicKey().PrivateKey;
+        }
+
+        public void SendMessage()
+        {
+            var client = new TcpClient("localhost", 443);
+            var sslStream = new SslStream(client.GetStream(), false, new RemoteCertificateValidationCallback(ValidateServerCertificate), null);
+            sslStream.AuthenticateAsClient("localhost");
+
+            var message = Encoding.UTF8.GetBytes("Hello from TLS Client");
+            sslStream.Write(message);
+            sslStream.Flush();
+
+            var buffer = new byte[2048];
+
+            int bytesRead = sslStream.Read(buffer, 0, buffer.Length);
+            var response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine("Received: " + response);
+            client.Close();
+        }
+
+        public static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            //return true; // For testing purposes, always accept the server certificate
+
+            if (sslPolicyErrors == SslPolicyErrors.None)
+            {
+                return true; // No errors, certificate is valid 
+            }
+
+            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+            // Check if the certificate is expired
+            if ((sslPolicyErrors & SslPolicyErrors.RemoteCertificateNotAvailable) != 0)
+            {
+                Console.WriteLine("Certificate not available.");
+                return false;
+            }
+            // Check if the certificate chain is valid
+            if ((sslPolicyErrors & SslPolicyErrors.RemoteCertificateChainErrors) != 0)
+            {
+                foreach (X509ChainStatus status in chain.ChainStatus)
+                {
+                    if (status.Status == X509ChainStatusFlags.UntrustedRoot)
+                    {
+                        // TODO: ignore for now
+                        return true;
+                    }
+                    else if (status.Status != X509ChainStatusFlags.NoError)
+                    {
+                        Console.WriteLine("Chain error: {0}", status.StatusInformation);
+                        return false;
+                    }
+                }
+            }
+            // Check if the certificate name matches the server's hostname
+            if ((sslPolicyErrors & SslPolicyErrors.RemoteCertificateNameMismatch) != 0)
+            {
+                Console.WriteLine("Certificate name mismatch.");
+                return false;
+            }
+
+            // If you want to accept the certificate despite the errors, return true 
+            // For production, you should handle each error appropriately return
+            return false;
+        }
+    }
+
+    public class Server
+    {
+        public Server()
+        {
+            var certificate = new X509Certificate2($"{Environment.CurrentDirectory}\\Certificate\\certificate_new.pfx", "5je486MgXr7lnKnHLLzRT!FEfFc5");
+            var listener = new TcpListener(IPAddress.Any, 443);
+            listener.Start();
+            Console.WriteLine("TLS Server started...");
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+
+                // after catch request from client
+                var sslStream = new SslStream(client.GetStream(), false);
+                sslStream.AuthenticateAsServer(certificate, false, System.Security.Authentication.SslProtocols.Tls12, true);
+                var buffer = new byte[2048];
+                int bytesRead = sslStream.Read(buffer, 0, buffer.Length);
+
+                var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                Console.WriteLine("Received: " + message);
+
+                var response = Encoding.UTF8.GetBytes("Hello from TLS Server");
+                sslStream.Write(response);
+                sslStream.Flush();
+                client.Close();
+            }
+        }
+    }
+}
---
+++ RSAEncryptUtilities.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PoP_example
+{
+    public static class RSAEncryptUtilities
+    {
+        public static (RSAParameters PublicKey,RSAParameters PrivateKey) GeneratePrivateAndPublicKey()
+        {
+            RSAParameters publicKey;
+            RSAParameters privateKey;
+
+            if (KeyCanBeUsed())
+            {
+                publicKey = GetJsonKey();
+                privateKey = GetJsonKey(false);
+            }
+            else
+            {
+                using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+                {
+                    publicKey = rsa.ExportParameters(false);
+                    privateKey = rsa.ExportParameters(true);
+                }
+
+                ExportKey(publicKey);
+                ExportKey(privateKey, false);
+            }
+
+            return (publicKey, privateKey);
+        }
+
+        private static RSAParameters GetJsonKey(bool isPublicKey = true)
+        {
+            var keyFile = new FileInfo(GetFilePath(isPublicKey));
+            RSAParameters key = default;
+
+            if (keyFile.Exists)
+            {
+                string str = "";
+                using (var stream = keyFile.OpenText())
+                {
+                    str = stream.ReadToEnd();
+                }
+
+                key = JsonConvert.DeserializeObject<RSAParameters>(str);
+            }
+            else
+                throw new Exception("Key is null!");
+
+            return key;
+        }
+
+        private static void ExportKey(RSAParameters key, bool isPublicKey = true)
+        {
+            // create file info
+            // open or create using stream
+            // serialize key as json
+            // get byte array of json file
+            // write byte[] to stream, close it
+            FileInfo keyFile = new FileInfo(GetFilePath(isPublicKey));
+
+            using (FileStream fs = new FileStream(GetFilePath(isPublicKey), FileMode.OpenOrCreate))
+            {
+                var json = JsonConvert.SerializeObject(key);
+                byte[] bytes = Encoding.UTF8.GetBytes(json);
+
+                fs.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        private static bool KeyCanBeUsed(bool isPublicKey = true)
+        {
+            FileInfo key = new FileInfo(GetFilePath(isPublicKey));
+
+            if (key.Exists)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static string GetFilePath(bool isPublicKey = true)
+        {
+            return isPublicKey switch
+            {
+                true => $"{Environment.CurrentDirectory}\\Keys\\Rsa_publicKey.json",
+                false => $"{Environment.CurrentDirectory}\\Keys\\Rsa_privateKey.json",
+            };
+        }
+    }
+}
